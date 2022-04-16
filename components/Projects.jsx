e5f57fb579b7155d96e57b9c56b6dbc8ced79a8a
thrizzle <thrzl@terabyteis.me>
2022-04-16 16:36:44 -0400

    ⚡️ actually switch to preact this time

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,6 +1,7 @@
 // import useFetch from "react-async"
 import { useQuery } from 'react-query'
-import { Grid, Text, Card, Loading, Link } from '@nextui-org/react'
+import { Grid, Text, Card, Link } from '@nextui-org/react'
+import { render } from "preact"
 
 function Projects(d) {
   const { isLoading, er, data } = useQuery('repoData', async () => {
@@ -8,16 +9,15 @@ function Projects(d) {
     return res.json()
   })
 
-  if (isLoading || !data) return <Loading type="points-opacity" color="primary" lg="true" />
+  if (isLoading || !data) return <p>loading projects...</p>
 
   if (er) return alert('An error has occurred: ' + er.message)
-
-  return (
+  render(
     <>
       {data.map((repo) => (
-        <Grid xs className='w-1/2'>
+        <Grid xs className='w-1/2' key={repo}>
           <Link className='w-full h-full' href={repo.link}>
-            <Card className="bg-gray-800 shadow-2xl border-0 fontcalc h-full w-full" hoverable clickable wrap="wrap" >
+            <Card className="bg-gray-900 border-0 shadow-none fontcalc h-full w-full" wrap="wrap" >
               <Text h2 className="text-white font-bold">{repo.repo.toLowerCase()}</Text>
               <Text h3 className="text-white text-md truncate mb-7">{repo.description.toLowerCase()}</Text>
               <Text h5 className="text-white absolute font-bold text-sm bottom-3 right-5 clear-both">⭐ {repo.stars}</Text>
@@ -27,7 +27,7 @@ function Projects(d) {
         </Grid>
       ))}
     </>
-  )
+    , document.getElementById("projects"))
 }
 
 export default Projects
--- next.config.js
+++ next.config.js
@@ -3,11 +3,26 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
 
-module.exports = withBundleAnalyzer({
-  reactStrictMode: true,
+const withPreact = require("next-plugin-preact")
+
+module.exports = withBundleAnalyzer(withPreact({
+  // reactStrictMode: true,
   images: {
-    domains: ['cdn.discordapp.com', 'media.discordapp.net', 'raw.githubusercontent.com', 'i.scdn.co'],
+    domains: ['cdn.discordapp.com', 'media.discordapp.net', 'raw.githubusercontent.com', 'i.scdn.co', "avatars.githubusercontent.com"],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
   },
-})
+  webpack: (config, { dev, isServer }) => {
+    // Replace React with Preact only in client production build
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        'react': 'preact/compat',
+        // 'react-dom/test-utils': 'preact/test-utils',
+        'react-dom': 'preact/compat',
+        'react/jsx-runtime': 'preact/jsx-runtime'
+      });
+    }
+
+    return config;
+  },
+}))
