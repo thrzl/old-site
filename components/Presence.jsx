d4413317a02f31019dbac249756c9d6edf7a31f8
thrizzle <thrzl@terabyteis.me>
2022-04-18 14:52:37 -0400

    ⚡️ remove MORE nextjs stuff

--- components/Presence.jsx
+++ components/Presence.jsx
@@ -1,20 +1,20 @@
-import { Card } from '@nextui-org/react'
+import { render } from 'preact'
 
 function Presence({ presence }) {
-    if (presence && presence.details) {
-      return (
-        <Card className="hidden bg-cover bg-center lg:flex p-0 text-white max-w-xs min-h-max mt-5 place-content-center h-fit transition-all ease-in-out duration-500 no-repeat w-max bg-indigo-800 border-0 absolute left-5 bottom-5 overflow-clip" shadow hoverable bordered={false} css={{backgroundImage: `url(${presence.image})`}}>
-          <div className="justify-center presence min-h-full backdrop-blur-sm m-3 rounded-md p-5">
-            <div className="p-3 place-content-start text-left text-base">
-              <h3 className="mb-0.5 font-bold">{presence.name.toLowerCase()}</h3>
-              <h4 className="mb-0.5">{presence.details.toLowerCase()}</h4>
-              <h4 className="mb-0.5">{presence.state ? presence.state.toLowerCase() : null}</h4>
-            </div>
+  if (presence && presence.details) {
+    return (
+      <div className="hidden bg-cover bg-center lg:flex hover:-translate-y-1 text-white max-w-xs min-h-max rounded-lg place-content-center transition-all ease-in-out duration-500 no-repeat w-max bg-indigo-800 border-0 absolute left-5 bottom-5 overflow-clip" shadow hoverable bordered={false} style={{ backgroundImage: `url(${presence.image})` }}>
+        <div className="justify-center presence min-h-full backdrop-blur-sm backdrop-brightness-75 rounded-md p-5">
+          <div className="p-3 place-content-start text-left text-base">
+            <h3 className="mb-0.5 font-bold">{presence.name.toLowerCase()}</h3>
+            <h4 className="mb-0.5">{presence.details.toLowerCase()}</h4>
+            <h4 className="mb-0.5">{presence.state ? presence.state.toLowerCase() : null}</h4>
           </div>
-        </Card>
-      )
-    }
-    return null
+        </div>
+      </div>
+    )
   }
+  return null
+}
 
 export default Presence
--- components/Spotify.jsx
+++ components/Spotify.jsx
@@ -1,21 +1,21 @@
-import { Card } from "@nextui-org/react"
+import { render } from "preact"
 
-function Spotify({spotify}) {
+function Spotify({ spotify }) {
     if (spotify) {
-        return (
-            <Card className="hidden backdrop-blur-3xl bg-cover bg-center lg:flex text-white max-w-md min-h-max p-5 mt-5 place-content-center h-fit transition-all ease-in-out duration-500 no-repeat w-max bg-green-900 border-0 absolute right-5 bottom-5 overflow-clip" shadow hoverable bordered={false} css={{backgroundImage: `url(${spotify.album_art_url})`}}>
-                <div className="justify-center backdrop-blur-sm presence min-h-full overflow-clip rounded-md p-1">
-                    <a href={`https://open.spotify.com/track/${spotify.track_id}`} color="default">
-                        <div className="p-3 place-content-start text-left text-white">
+        render(
+            <div className="hidden bg-cover bg-center lg:flex hover:-translate-y-1 text-white min-h-max rounded-lg place-content-center transition-all ease-in-out duration-500 no-repeat min-w-max border-0 absolute right-5 bottom-5 overflow-clip" style={{ backgroundImage: `url(${spotify.album_art_url})` }}>
+                <div className="justify-center backdrop-blur-sm backdrop-brightness-75 presence min-h-full overflow-clip p-5 rounded-md">
+                    <a href={`https://open.spotify.com/track/${spotify.track_id}`} className="h-full">
+                        <div className="p-3 place-content-start text-left text-white min-h-full">
                             <h3 className="mb-0.5 font-bold truncate">listening to {spotify.song.toLowerCase()}</h3>
                             <h4 className="mb-0.5 truncate">on {spotify.album.toLowerCase()}</h4>
                             <h4 className="mb-0.5 truncate">by {spotify.artist.toLowerCase()}</h4>
                         </div>
                     </a>
                 </div>
-            </Card>
-        )
-    }  
+            </div>
+            , document.getElementById("spotify"))
+    }
     return (
         null
     )
