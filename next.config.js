e84a4fd2fde9c6dec865961c9c33aab0780a06bc
terabyte3 <terry.bazemoreiii@gmail.com>
2022-06-03 23:28:19 -0400

    🚑 stuff not building

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-})
+// const withBundleAnalyzer = require('@next/bundle-analyzer')({
+//   enabled: process.env.ANALYZE === 'true',
+// })
 
 const withPreact = require("next-plugin-preact")
 
-module.exports = withBundleAnalyzer(withPreact({
+module.exports = (withPreact({
   swcMinify: true,
   experimental: { esmExternals: false },
   // reactStrictMode: true,
