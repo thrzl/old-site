fbac43785063686e49ec4e7e711881304ddff0f7
thrizzle <thrzl@terabyteis.me>
2022-06-23 18:04:47 +0000

    ♻️ svelte

---
+++ src/Moosync.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	import { useSWR } from 'sswr';
+	const { data: activity } = useSWR('https://api.lanyard.rest/v1/users/536644802595520534');
+	var imgUrl: string;
+	var artist: string = '';
+	var album: string = '';
+	$: act = $activity ? $activity.data.activities : null;
+	$: moosyncData = act ? act.filter((a) => a.name.toLowerCase() == 'moosync') : null;
+	$: if (moosyncData) {
+		[artist, album] = moosyncData[0].state.split(' - ');
+		imgUrl = moosyncData[0].assets.large_image.replace(
+			'mp:external/',
+			'https://media.discordapp.net/external/'
+		);
+	}
+</script>
+
+{#if moosyncData && moosyncData.length}
+	<div
+		class="hidden bg-cover bg-neutral-900 bg-center lg:flex hover:-translate-y-1 text-white min-h-max rounded-lg place-content-center transition-all ease-in-out duration-500 no-repeat min-w-max border-0 absolute right-5 bottom-5 overflow-clip"
+		style={`background-image: url(${imgUrl})`}
+	>
+		<div
+			class="justify-center backdrop-blur-sm backdrop-brightness-75 presence min-h-full overflow-clip p-5 rounded-md"
+		>
+			<div class="p-3 place-content-start text-left text-white min-h-full">
+				<h3 class="mb-0.5 font-bold truncate">
+					listening to {moosyncData[0].details ? moosyncData[0].details.toLowerCase() : null}
+				</h3>
+				<h4 class="mb-0.5 truncate">on {album.toLowerCase()}</h4>
+				<h4 class="mb-0.5 truncate">by {artist.toLowerCase()}</h4>
+			</div>
+		</div>
+	</div>
+{/if}
---
+++ src/routes/index.svelte
@@ -0,0 +1,52 @@
+<script>
+	import profilePic from '../../static/logo192.webp';
+	import Projects from '../Projects.svelte';
+	import Presence from '../Presence.svelte';
+	import Moosync from '../Moosync.svelte';
+</script>
+
+<div class="App bg-black">
+	<div id="moosync">
+		<Moosync />
+	</div>
+	<div id="presence">
+		<Presence />
+	</div>
+	<header class="App-header m-auto">
+		<img
+			width="160px"
+			height="160px"
+			src={profilePic}
+			class="rounded-full relative"
+			alt="profile"
+		/>
+		<h1
+			class="headertext my-2 font-bold sm:tagline text-gradient bg-gradient-to-r from-blue-500 to-purple-500 m-0 sm:text-small"
+		>
+			heyo, i&apos;m thrizzle.
+		</h1>
+		<div class="links flex text-base align-middle font-mono h-min-content">
+			<a
+				href="https://github.com/terabyte3"
+				class="text-white text-gradient p-3 from-gray-600 to-white bg-gradient-to-br transition-all ease-in-out duration-500 hover:font-boldhover:from-white hover:to-gray-600 mx-3 leading-tight font-mono"
+			>
+				/gh
+			</a>
+			<a
+				href="https://fermi.terabyteis.me"
+				class="text-white text-gradient p-3 from-purple-600 to-indigo-400 bg-gradient-to-br transition-all ease-in-out duration-500 hover:from-indigo-400 hover:to-purple-600 mx-3 leading-tight font-mono"
+			>
+				/fermi
+			</a>
+			<a
+				href="https://crust.terabyteis.me"
+				class="text-white text-gradient p-3 from-orange-600 to-orange-400 bg-gradient-to-br transition-all ease-in-out duration-500 hover:from-orange-400 hover:to-orange-600 mx-3 leading-tight font-mono"
+			>
+				/crust
+			</a>
+		</div>
+		<div class="flex w-1/2 items-center" id="projects">
+			<Projects />
+		</div>
+	</header>
+</div>
