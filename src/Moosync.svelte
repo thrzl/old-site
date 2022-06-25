7b941bc1da3dec2cf5af20b936e0058062ba03e9
terabyte3 <terry.bazemoreiii@gmail.com>
2022-06-24 22:46:57 -0400

    ⚡️ better code

--- src/Moosync.svelte
+++ src/Moosync.svelte
@@ -5,10 +5,10 @@
 	var artist: string = '';
 	var album: string = '';
 	$: act = $activity ? $activity.data.activities : null;
-	$: moosyncData = act ? act.filter((a) => a.name.toLowerCase() == 'moosync') : null;
+	$: moosyncData = act ? act.filter((a) => a.name.toLowerCase() == 'moosync')[0] : null;
 	$: if (moosyncData) {
-		[artist, album] = moosyncData[0].state.split(' - ');
-		imgUrl = moosyncData[0].assets.large_image.replace(
+		[artist, album] = moosyncData.state.split(' - ');
+		imgUrl = moosyncData.assets.large_image.replace(
 			'mp:external/',
 			'https://media.discordapp.net/external/'
 		);
@@ -17,7 +17,7 @@
 
 {#if moosyncData && moosyncData.length}
 	<div
-		class="hidden bg-cover bg-neutral-900 bg-center lg:flex hover:-translate-y-1 text-white min-h-max rounded-lg place-content-center transition-all ease-in-out duration-500 no-repeat min-w-max border-0 absolute right-5 bottom-5 overflow-clip"
+		class="hidden bg-cover bg-neutral-900 bg-center lg:flex hover:-translate-y-1 text-white min-h-max rounded-lg place-content-center transition-all ease-in-out duration-500 no-repeat min-w-md border-0 absolute right-5 bottom-5 overflow-clip"
 		style={`background-image: url(${imgUrl})`}
 	>
 		<div
@@ -25,9 +25,9 @@
 		>
 			<div class="p-3 place-content-start text-left text-white min-h-full">
 				<h3 class="mb-0.5 font-bold truncate">
-					listening to {moosyncData[0].details ? moosyncData[0].details.toLowerCase() : null}
+					listening to {moosyncData.details ? moosyncData.details.toLowerCase() : null}
 				</h3>
-				<h4 class="mb-0.5 truncate">on {album.toLowerCase()}</h4>
+				{#if album}<h4 class="mb-0.5 truncate">on {album.toLowerCase()}</h4>{/if}
 				<h4 class="mb-0.5 truncate">by {artist.toLowerCase()}</h4>
 			</div>
 		</div>
--- src/Presence.svelte
+++ src/Presence.svelte
@@ -1,64 +1,54 @@
 <script lang="ts">
-    import { useSWR } from "sswr";
-    const {data: activity} = useSWR("https://api.lanyard.rest/v1/users/536644802595520534")
-    $: act = $activity ? $activity.data : null
-    interface Presence {
-        image: string,
-        id: BigInt,
-        name: string,
-        state: string,
-        details: string,
-        large_text: string
-    }
-    let p: Presence;
-    $: if (act && act.activities.length) {
-      for (const i in act.activities) {
-        let activity = act.activities[i]
-        if (activity.type == 0) {
-          let img;
-          if (activity.assets) { // find the appropriate image
-            if (activity.assets.large_image) {
-              img = activity.assets.large_image
-            } else if (activity.assets.small_image) {
-              img = activity.assets.small_image
-            } else {
-              img = null
-            }
-          } else {
-            img = null
-          }
-          let imgUrl;
-          if (img) { // discord images are really wierd, we need to parse it to get the actual url
-            if (img.includes("mp:external/")) {
-              imgUrl = img.replace("mp:external/", "https://media.discordapp.net/external/")
-            } else {
-              imgUrl = `https://cdn.discordapp.com/app-assets/${activity.application_id}/${img}.png?size=300`
-            }
-          } else {
-            imgUrl = "https://raw.githubusercontent.com/NYRI4/Discolored/e0a63e07fbfee073483210feb7778245addb0ce2/assets/arcades.svg"
-          }
-          p = {
-            image: imgUrl,
-            id: activity.application_id,
-            name: activity.name,
-            state: activity.state,
-            details: activity.details,
-            large_text: activity.assets ? activity.assets.large_text : null
-          }
-          break
-        }
-      }
-    }
+	import { useSWR } from 'sswr';
+	const { data: activity } = useSWR('https://api.lanyard.rest/v1/users/536644802595520534');
+	$: act = $activity ? $activity.data.activities[0] : null;
+	interface Presence {
+		image: string;
+		id: BigInt;
+		name: string;
+		state: string;
+		details: string;
+		large_text: string;
+	}
+	let p: Presence;
+	$: if (act) {
+		let activity = act;
+		if (activity.type == 0) {
+			let img = activity.assets ? activity.assets.large_image || activity.assets.small_image : null;
+			let imgUrl =
+				'https://raw.githubusercontent.com/NYRI4/Discolored/e0a63e07fbfee073483210feb7778245addb0ce2/assets/arcades.svg';
+			if (img) {
+				// discord images are really wierd, we need to parse it to get the actual url
+				imgUrl = `https://cdn.discordapp.com/app-assets/${activity.application_id}/${img}.png?size=300`;
+				if (img.includes('mp:external/')) {
+					imgUrl = img.replace('mp:external/', 'https://media.discordapp.net/external/');
+				}
+			}
+			p = {
+				image: imgUrl,
+				id: activity.application_id,
+				name: activity.name,
+				state: activity.state,
+				details: activity.details,
+				large_text: activity.assets ? activity.assets.large_text : null
+			};
+		}
+	}
 </script>
 
-{#if p && p.name.toLowerCase() !== "moosync"}
-<div class="hidden bg-cover bg-center lg:flex hover:-translate-y-1 text-white max-w-xs min-h-max rounded-lg place-content-center transition-all ease-in-out duration-500 no-repeat w-max bg-indigo-800 border-0 absolute left-5 bottom-5 overflow-clip" style={`background-image: url(${p.image})`}>
-    <div class="justify-center presence min-h-full backdrop-blur-sm backdrop-brightness-75 rounded-md p-5">
-        <div class="p-3 place-content-start text-left text-base">
-        <h3 class="mb-0.5 font-bold">{p.name.toLowerCase()}</h3>
-        <h4 class="mb-0.5">{p.details.toLowerCase()}</h4>
-        <h4 class="mb-0.5">{p.state ? p.state.toLowerCase() : null}</h4>
-        </div>
-    </div>
-</div>
+{#if p && p.name.toLowerCase() !== 'moosync'}
+	<div
+		class="hidden bg-cover bg-center lg:flex hover:-translate-y-1 text-white max-w-xs min-h-max rounded-lg place-content-center transition-all ease-in-out duration-500 no-repeat w-max bg-indigo-800 border-0 absolute left-5 bottom-5 overflow-clip"
+		style={`background-image: url(${p.image})`}
+	>
+		<div
+			class="justify-center presence min-h-full backdrop-blur-sm backdrop-brightness-75 rounded-md p-5"
+		>
+			<div class="p-3 place-content-start text-left text-base">
+				<h3 class="mb-0.5 font-bold">{p.name.toLowerCase()}</h3>
+				<h4 class="mb-0.5">{p.details.toLowerCase()}</h4>
+				<h4 class="mb-0.5">{p.state ? p.state.toLowerCase() : null}</h4>
+			</div>
+		</div>
+	</div>
 {/if}
