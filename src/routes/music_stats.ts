9e20059731d6ddd19fd92e2eb12f54caeff44347
terabyte3 <terry.bazemoreiii@gmail.com>
2022-07-13 22:37:23 -0400

    🍱 more image fixes

--- src/routes/music_stats.ts
+++ src/routes/music_stats.ts
@@ -1,4 +1,6 @@
 /** @type {import('@sveltejs/kit').RequestHandler} */
+import musicNote from "../static/music.webp"
+
 export async function get() {
 	const topArtistsRes = await fetch(`http://ws.audioscrobbler.com/2.0/?method=user.getTopArtists&api_key=${process.env.LASTFM_API_KEY}&format=json&period=1week&limit=1&user=thrzl`);
 	const topArtists = await topArtistsRes.json();
@@ -8,7 +10,7 @@ export async function get() {
 		const track = topTracks.toptracks.track[trackn];
 		const info = await fetch(`http://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key=${process.env.LASTFM_API_KEY}&format=json&artist=${track.artist.name}&track=${track.name}`);
 		const i = await info.json();
-		track.image = i.track?.album?.image?.[3]["#text"] || "../../static/music.webp";
+		track.image = i.track?.album?.image?.[3]["#text"] || musicNote;
 		topTracks.toptracks.track[trackn] = track;
 	}
 	return {
