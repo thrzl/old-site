f79072bf181cf5df18fb678fb010a28635bea1db
terabyte3 <terry.bazemoreiii@gmail.com>
2022-09-03 22:45:08 -0400

    ⚡️

--- src/routes/api/music.ts
+++ src/routes/api/music.ts
@@ -1,27 +1,19 @@
 /** @type {import('@sveltejs/kit').RequestHandler} */
 
 export async function GET() {
-	const topArtistsRes = await fetch(
-		`https://ws.audioscrobbler.com/2.0/?method=user.getTopArtists&api_key=${process.env.LASTFM_API_KEY}&format=json&period=1week&limit=1&user=thrzl`
+	const data = await fetch(
+		`https://last.thrzl.xyz/thrzl`
 	);
-	const topArtists = await topArtistsRes.json();
-	const topTracksRes = await fetch(
-		`https://ws.audioscrobbler.com/2.0/?method=user.getTopTracks&api_key=${process.env.LASTFM_API_KEY}&format=json&period=1week&limit=24&user=thrzl`
-	);
-	const topTracks = await topTracksRes.json();
-	for (const trackn in topTracks.toptracks.track) {
-		const track = topTracks.toptracks.track[trackn];
-		const info = await fetch(
-			`https://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key=${process.env.LASTFM_API_KEY}&format=json&artist=${track.artist.name}&track=${track.name}`
-		);
-		const i = await info.json();
-		track.image = i.track?.album?.image?.[3]['#text'];
-		topTracks.toptracks.track[trackn] = track;
+	const d = await data.json();
+	for (const trackn in d.top_tracks) {
+		const track = d.top_tracks[trackn];
+		track.image = !d.top_tracks[trackn]?.image?.[3]["#text"].includes("2a96cbd8b46e442fc41c2b86b821562f") ? d.top_tracks[trackn]?.image?.[3]['#text']: null;
+		d.top_tracks[trackn] = track;
 	}
 	return {
 		body: {
-			topArtist: topArtists.topartists.artist[0],
-			topTracks: topTracks.toptracks.track
+			topArtist: d.top_artist,
+			topTracks: d.top_tracks
 		}
 	};
 }
