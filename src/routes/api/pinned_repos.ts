a910796d051d1c69edf4dfafb605b8169ee18734
terabyte3 <terry.bazemoreiii@gmail.com>
2022-09-14 17:42:54 -0400

    🩹 fix projects page

--- src/routes/api/pinned_repos.ts
+++ src/routes/api/pinned_repos.ts
@@ -1,6 +1,6 @@
 /** @type {import('@sveltejs/kit').RequestHandler} */
 export async function GET() {
-	const res = await fetch('https://pinned.thrzl.xyz/terabyte3');
+	const res = await fetch('https://pinned.thrzl.xyz/thrzl');
 	if (res) {
 		const repos = await res.json();
 		return {
