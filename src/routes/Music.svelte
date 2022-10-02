c5fd1be534e342530c9cfab7acbeed67b0fb54f3
terabyte3 <terry.bazemoreiii@gmail.com>
2022-10-02 16:56:30 -0400

    ✨ increase length of music

--- src/routes/Music.svelte
+++ src/routes/Music.svelte
@@ -23,9 +23,9 @@
 			class="relative w-screen ml-[calc(-50vw)] left-1/2 mb-12 mt-2 [mask-image:linear-gradient(to_right,#000_0%,#000_75%,#0000_100%)] lg:[mask-image:linear-gradient(to_right,#0000_0%,#000_25%,#000_75%,#0000_100%)] before:z-10"
 		>
 			<div
-				class="lg:px-[calc(50vw-400px)] md:px-[calc(50vw-350px)] px-8 lg:scroll-pl-[calc(50vw-400px)] md:scroll-pl-[calc(50vw-350px)] scroll-pl-8 grid grid-rows-2 grid-cols-[repeat(12,calc(9rem))] md:grid-cols-[repeat(12,calc(12rem))] lg:grid-cols-[repeat(12,calc(14rem))] grid-flow-col gap-4 overflow-x-scroll no-scrollbar snap-x snap-mandatory"
+				class="lg:px-[calc(50vw-400px)] md:px-[calc(50vw-350px)] px-8 lg:scroll-pl-[calc(50vw-400px)] md:scroll-pl-[calc(50vw-350px)] scroll-pl-8 grid grid-rows-2 grid-cols-[repeat(24,calc(9rem))] md:grid-cols-[repeat(24,calc(12rem))] lg:grid-cols-[repeat(24,calc(14rem))] grid-flow-col gap-4 overflow-x-scroll no-scrollbar snap-x snap-mandatory"
 			>
-				{#each [...Array(24)] as _, i}
+				{#each [...Array(48)] as _, i}
 					<a
 						href="https://last.fm"
 						target="_blank"
@@ -73,7 +73,7 @@
 			class="relative w-screen ml-[calc(-50vw)] left-1/2 mb-12 mt-2 [mask-image:linear-gradient(to_right,#000_0%,#000_75%,#0000_100%)] lg:[mask-image:linear-gradient(to_right,#0000_0%,#000_25%,#000_75%,#0000_100%)] before:z-10"
 		>
 			<div
-				class="lg:px-[calc(50vw-400px)] md:px-[calc(50vw-350px)] px-8 lg:scroll-pl-[calc(50vw-400px)] md:scroll-pl-[calc(50vw-350px)] scroll-pl-8 grid grid-rows-2 grid-cols-[repeat(12,calc(9rem))] md:grid-cols-[repeat(12,calc(12rem))] lg:grid-cols-[repeat(12,calc(14rem))] grid-flow-col gap-4 overflow-x-scroll no-scrollbar snap-x snap-mandatory"
+				class="lg:px-[calc(50vw-400px)] md:px-[calc(50vw-350px)] px-8 lg:scroll-pl-[calc(50vw-400px)] md:scroll-pl-[calc(50vw-350px)] scroll-pl-8 grid grid-rows-2 grid-cols-[repeat(24,calc(9rem))] md:grid-cols-[repeat(24,calc(12rem))] lg:grid-cols-[repeat(24,calc(14rem))] grid-flow-col gap-4 overflow-x-scroll no-scrollbar snap-x snap-mandatory"
 			>
 				{#each lastfm.topTracks as track, i}
 					<a
