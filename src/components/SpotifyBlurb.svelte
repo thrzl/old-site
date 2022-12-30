75b8fad4de59395414ff91133cea734147b66630
--global <--global>
2022-12-30 12:34:31 -0500

    🩹 humanize artist list

--- src/components/SpotifyBlurb.svelte
+++ src/components/SpotifyBlurb.svelte
@@ -7,9 +7,23 @@
     function endsWithPunctuation(str: string) {
         return str.endsWith(".") || str.endsWith("!") || str.endsWith("?")
     }
-    let musicBlurb = `${`on ${music?.album} ` ? music?.song !== music?.album : " "} by ${music?.artist}`
+    function humanizeList (list: Array) {
+        if (list.length === 1) {return list[0]}
+        if (list.length === 2) {return `${list[0]} and ${list[1]}`}
+
+        let humanizedList = ''
+        for (let i = 0; i < list.length; i++) {
+            if (i === list.length - 1) {
+                humanizedList += ', and '
+            } else if (i !== 0) {
+                humanizedList += ', '
+            }
+            humanizedList += list[i]
+        }
+        return humanizedList
+    }
 </script>
 
 {#if music}
-<span class="lowercase">listening to <a class="text-green" href={`https://open.spotify.com/track/${music.track_id}`}>{music.song}</a> by {music.artist}{#if music.song !== music.album} on {music.album}{/if}{#if !endsWithPunctuation(music.album)}.{/if}</span>
+<span class="lowercase">listening to <a class="text-green" href={`https://open.spotify.com/track/${music.track_id}`}>{music.song}</a> by {humanizeList(music.artist.split("; "))}{#if music.song !== music.album}{' '}on {music.album}{/if}{#if !endsWithPunctuation(music.album)}.{/if}</span>
 {/if}
